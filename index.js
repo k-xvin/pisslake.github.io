49c9a1ba61c011a9b22d0955434e3ef130d8d8c9
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-01-15 11:47:38 -0800

    removed old code

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// called when page loaded
-/*document.addEventListener('DOMContentLoaded', function(e){
-
-	setupListeners();
-	console.log("dom content loaded");
-});*/
-
 function setupListeners(){
 	pissbtn = document.getElementById("pissbtn");
 	pissbtn.addEventListener("click", onPissClick);
